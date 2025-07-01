effbc611098aeaf7f1db8511222817a1f1321c3b
Gu Xinlu <153700159+clementineyyy@users.noreply.github.com>
2025-07-01 18:46:35 +0800

    Delete E22314049谷昕璐(单周)第二次大作业/E22314049谷昕璐(单周)实验三 directory

--- E22314049谷昕璐(单周)第二次大作业/E22314049谷昕璐(单周)实验三/ClientLauncher.java
+++
@@ -1,9 +0,0 @@
-public class ClientLauncher {
-    public static void main(String[] args) throws Exception {
-        // 启动第一个客户端
-        Process p1 = Runtime.getRuntime().exec("java -cp src Client");
-
-        // 等待一段时间再启动其他客户端
-        Thread.sleep(1500);
-    }
-}
